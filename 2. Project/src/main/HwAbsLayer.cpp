be57a849057b5ff94c0fe7e406ae90f91eaaa2ef
Gabriel <gabriel.idalgo@gmail.com>
2020-02-11 21:53:54 -0300

    incluindo HwAbsLayer

---
+++ 2. Project/src/main/HwAbsLayer.cpp
@@ -0,0 +1,10 @@
+/*
+ * HwAbsLayer.cpp
+ *
+ *  Created on: 1 de fev de 2020
+ *      Author: Gabriel
+ */
+
+
+
+
---
+++ 2. Project/src/main/HwAbsLayer.h
@@ -0,0 +1,15 @@
+/*
+ * HwAbsLayer.h
+ *
+ *  Created on: 1 de fev de 2020
+ *      Author: Gabriel
+ */
+
+#ifndef HWABSLAYER_H_
+#define HWABSLAYER_H_
+
+
+
+
+
+#endif /* HWABSLAYER_H_ */
